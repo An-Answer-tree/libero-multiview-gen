84f970c66fdb66ea50c430e88b5dede018c61b67
Tong Liu <szliutong@gmail.com>
2026-03-03 10:25:12 +0800

    [set up env]

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ conda install nvidia::cuda-toolkit==13.0.0
 uv pip install -r requirements.txt
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu130
 
-git clone --branch v1.4.0 https://github.com/ARISE-Initiative/robosuite.git
-CC=/usr/bin/gcc uv pip install -e ../robosuite
+# git clone --branch v1.4.0 https://github.com/ARISE-Initiative/robosuite.git
+CC=/usr/bin/gcc uv pip install -e ./third_party/robosuite
 uv pip install -e .
+
+python benchmark_scripts/download_libero_datasets.py --use-huggingface
